d52c8dbca83d76c62433c2d849bda71044a78640
k1LoW <k1lowxb@gmail.com>
2021-10-06 14:54:15 +0900

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-ghput tag --release --tag=${GHPUT_TAG} --tag-time-format=${GHPUT_TAG_TIME_FORMAT} --tag-release-title="${GHPUT_TAG_RELEASE_TITLE}" --tag-release-body="${GHPUT_TAG_RELEASE_BODY}"
+ghput tag --release --tag=${GHPUT_TAG} --tag-time-format=${GHPUT_TAG_TIME_FORMAT} --release-title="${GHPUT_RELEASE_TITLE}" --release-body="${GHPUT_RELEASE_BODY}"
